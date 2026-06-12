e99a2c97dd437320f463895c1a096c57e31f6425
sonugupta1234 <sonuguptahld037@gmail.com>
2026-06-12 12:53:08 +0530

    build

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:7780/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 axiosInstance.interceptors.request.use(
